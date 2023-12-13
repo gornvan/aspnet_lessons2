f76b394a139ca14749c8c39fbf458ac43499859b
gornvan <javanlini@gmail.com>
2023-12-13 19:38:05 +0100

    Cat: implement IHunter, also add WantoToRun & OfferFood

--- lesson2 - unit testing/lesson2 - unit testing/Cat.cs
+++ lesson2 - unit testing/lesson2 - unit testing/Cat.cs
@@ -1,12 +1,58 @@
-﻿namespace lesson2_unit_testing
+﻿using lesson2___unit_testing.Contracts;
+
+namespace lesson2_unit_testing
 {
-    public class Cat
+    public class Cat : IHunter
     {
+        private double _funRunDistance;
+        private double _speed;
+
+        public Cat( double funRunDistance, double speed ) {
+            _funRunDistance = funRunDistance;
+            _speed = speed;
+        }
 
         public void Voice()
         {
             Console.WriteLine("Meow!");
         }
 
+
+        public bool OfferFood(Food food)
+        {
+            switch (food)
+            {
+                case Food.Fruit:
+                case Food.Chips:
+                    return false;
+
+                case Food.Meat:
+                case Food.Fish:
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        public bool WantToRun(double[] DistancesInMetersToRun)
+        {
+            var totalDistance = DistancesInMetersToRun.Aggregate(
+                (double)0.0d, // initialization of the aggreation
+                (sum/*current result*/, current/*current aggregated value*/) 
+                    => sum + current // return new result
+            );
+
+            return totalDistance <= _funRunDistance;
+        }
+
+        public bool CanCatch(IPrey prey)
+        {
+            return prey.GetSpeed_MetersPerSecond() < _speed;
+        }
+
+        public double GetSpeed_MetersPerSecond() => _speed;
+
+        public double Speed_MetersPerSecond => _speed;
     }
 }
