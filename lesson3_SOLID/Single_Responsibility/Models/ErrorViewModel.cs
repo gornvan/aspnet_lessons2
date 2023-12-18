f04d8b6c534dfe5b9af529bcb9db1fcb95396683
gornvan <javanlini@gmail.com>
2023-12-18 17:55:30 +0100

    Create an mvc app to demonstrate SingleResponsibility-related Anti-Pattern

---
+++ lesson3_SOLID/Single_Responsibility/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Single_Responsibility.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
