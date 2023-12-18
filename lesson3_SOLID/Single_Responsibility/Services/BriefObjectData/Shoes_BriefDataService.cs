4a07fab7d1d369c78a6f5f4b3d337320d4831fc8
gornvan <javanlini@gmail.com>
2023-12-18 18:42:26 +0100

    Demonstrate Liskov's Substitution anti-pattern - add the violating class

---
+++ lesson3_SOLID/Single_Responsibility/Services/BriefObjectData/Shoes_BriefDataService.cs
@@ -0,0 +1,25 @@
+﻿namespace SOLID.Services.BriefObjectData
+{
+    public class Shoes_BriefDataService : BriefObjectDataServiceBase
+    {
+        // pretend to be External Storage
+        public string[] Data =
+        {
+            "Sneakers",
+            "Red",  // This will become a problem
+                    // when we'll want to generalize our solution
+                    // which presents / verifies / converts such data for Customer
+
+            "Shoes",
+            "Black",
+
+            "Clappers",
+            "Blue",
+        };
+
+        public override string[] GetObjectsBriefData()
+        {
+            return Data;
+        }
+    }
+}
