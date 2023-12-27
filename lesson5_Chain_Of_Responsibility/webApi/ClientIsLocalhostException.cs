268b69c99a553bf4e84bb8706103b54f03d9cec3
gornvan <javanlini@gmail.com>
2023-12-27 18:07:09 +0100

    Create an example of an EndpointFilter to demonstrate a module of a Chain of Responsibility

---
+++ lesson5_Chain_Of_Responsibility/webApi/ClientIsLocalhostException.cs
@@ -0,0 +1,17 @@
+﻿namespace lesson5_Chain_Of_Responsibility
+{
+    public class ClientIsLocalhostException : Exception
+    {
+        public ClientIsLocalhostException() : base()
+        {
+        }
+
+        public ClientIsLocalhostException(string? message) : base(message)
+        {
+        }
+
+        public ClientIsLocalhostException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ lesson5_Chain_Of_Responsibility/webApi/LocalhostActionFilter.cs
@@ -0,0 +1,18 @@
+﻿namespace lesson5_Chain_Of_Responsibility
+{
+    public class LocalhostActionFilter : IEndpointFilter
+    {
+        public ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            // this is not a reliable way to check if the client is localhost!
+            var clientIsLocalhost = context.HttpContext.Connection.RemoteIpAddress.ToString() == "127.0.0.1";
+
+            if (clientIsLocalhost)
+            {
+                throw new ClientIsLocalhostException("The client is not from localhost!");
+            }
+
+            return next(context);
+        }
+    }
+}
