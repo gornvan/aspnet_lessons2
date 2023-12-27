ee0890d96004af0b0866b6daa2f98aafffe891d3
gornvan <javanlini@gmail.com>
2023-12-27 19:08:40 +0100

    add an example of chain of responsibility in form of MVC action filter

---
+++ lesson5_Chain_Of_Responsibility/mvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using lesson5_Chain_Of_Responsibility;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [LocalhostActionFilter]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ lesson5_Chain_Of_Responsibility/mvc/Filters/LocalHostActionFilter/LocalhostActionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace lesson5_Chain_Of_Responsibility
+{
+    public class LocalhostActionFilterAttribute : ActionFilterAttribute
+    {
+        // THIS CODE IS CLONED IN THE WEB API SYSTEM (../webapi)
+        // TODO: pull it into a nuget package
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            // this is not a reliable way to check if the client is localhost!
+            var clientAddress = context?.HttpContext?.Connection?.RemoteIpAddress?.ToString() ?? string.Empty;
+            var clientIsLocalhost = clientAddress == "127.0.0.1";
+
+            if (clientIsLocalhost)
+            {
+                throw new ClientIsLocalhostException("The client is not from localhost!");
+            }
+        }
+    }
+}
