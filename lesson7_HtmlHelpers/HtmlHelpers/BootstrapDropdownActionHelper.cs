c13e651fbb30673eeb517cd1caf3c9de68e3dbb8
gornvan <javanlini@gmail.com>
2024-01-15 08:55:30 +0100

    create a couple of html helpers for demonstratoin and use them on the index view

---
+++ lesson7_HtmlHelpers/HtmlHelpers/BootstrapDropdownActionHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text;
+
+namespace lesson7_HtmlHelpers.HtmlHelpers
+{
+    public static class BootstrapDropdownActionHelper
+    {
+        const string _template_without_lis =
+                """
+            <div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown button
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                {0}
+              </ul>
+            </div>
+            """;
+
+        /// <summary>
+        /// Do not confuse with Dropdown Select!
+        /// This is for selectable ACTION, not for selecting data.
+        /// </summary>
+        public static HtmlString CreateDropdownActoinButton(this IHtmlHelper html, string[] items)
+        {
+            StringBuilder listItems = new StringBuilder();
+
+            foreach (var item in items)
+            {
+                listItems.Append("<li class=\"dropdown-item\" href=\"#\">");
+                listItems.Append(item);
+                listItems.Append("</li>");
+            }
+
+            string dropdownHtml = string.Format(_template_without_lis, listItems.ToString());
+
+            return new HtmlString(dropdownHtml);
+        }
+    }
+}
---
+++ lesson7_HtmlHelpers/HtmlHelpers/ListHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text;
+
+namespace lesson7_HtmlHelpers.HtmlHelpers
+{
+	public static class ListHelper
+	{
+        public static HtmlString CreateList(this IHtmlHelper html, string[] items)
+        {
+            StringBuilder resultBuilder = new StringBuilder("<ul>");
+
+            foreach (string item in items)
+            {
+                resultBuilder.Append($"<li>");
+                resultBuilder.Append(item);
+                resultBuilder.Append($"</li>");
+            }
+
+            resultBuilder.Append("</ul>");
+
+            return new HtmlString(resultBuilder.ToString());
+        }
+
+    }
+}
