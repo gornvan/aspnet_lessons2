735c45c377785ebcef4da750146980331f231214
gornvan <javanlini@gmail.com>
2024-01-17 21:03:01 +0100

    Create a console Program which uses the generated API client

---
+++ lesson8_WebAPI/client/Program.cs
@@ -0,0 +1,11 @@
+﻿using IO.Swagger.Api;
+using IO.Swagger.Client;
+using System.Text.Json;
+
+var client = new ApiClient();
+
+var api = new WeatherForecastApi(client);
+
+var forecasts = api.GetWeatherForecast(10, 15, 10, 45, null);
+
+Console.WriteLine(JsonSerializer.Serialize(forecasts));
