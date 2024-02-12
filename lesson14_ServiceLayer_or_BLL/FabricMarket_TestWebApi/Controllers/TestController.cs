3613227e06084d056afbb5d565c0cf1ea582fe06
gornvan <javanlini@gmail.com>
2024-02-12 20:24:41 +0100

    Create SystemSettings controller

--- lesson14_ServiceLayer_or_BLL/FabricMarket_TestWebApi/Controllers/TestController.cs
+++ lesson14_ServiceLayer_or_BLL/FabricMarket_TestWebApi/Controllers/TestController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 namespace FabricMarket_TestWebApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class TestController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
