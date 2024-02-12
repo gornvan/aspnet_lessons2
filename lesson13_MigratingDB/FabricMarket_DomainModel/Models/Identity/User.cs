c34917d0fe655e1c0ba12909d83d0d68b5b5c149
gornvan <javanlini@gmail.com>
2024-02-12 17:26:30 +0100

    Restrict User's text fields with MaxLength Attributes

--- lesson13_MigratingDB/FabricMarket_DomainModel/Models/Identity/User.cs
+++ lesson13_MigratingDB/FabricMarket_DomainModel/Models/Identity/User.cs
@@ -1,12 +1,21 @@
-﻿namespace lesson11_FabricMarket_DomainModel.Models.Identity
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace lesson11_FabricMarket_DomainModel.Models.Identity
 {
     public class User : Entity<long>
     {
+        [MaxLength(100)]
         public required string FirstName { get; set; }
+        [MaxLength(100)]
         public string? LastName { get; set; }
+        [MaxLength(200)]
         public required string Email { get; set; }
         public required UserRoleEnum Role { get; set; }
+
+        [MaxLength(100)]
         public required string PasswordHash { get; set; }
+        [MaxLength(100)]
         public required string PasswordSalt { get; set; }
 
         public required UserSettings UserSettings { get; set; }
