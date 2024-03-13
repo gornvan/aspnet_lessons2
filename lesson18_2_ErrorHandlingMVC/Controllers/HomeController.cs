febf156558c3f8556fc5b46cef9664b753e51dee
gornvan <javanlini@gmail.com>
2024-03-13 18:21:53 +0100

    Create error handling page and use it for non-development runs Use /Home/Privacy as the exception page configure http-production to start with non-development environment add a picture from colorlib.com/wp/free-404-error-page-templates/

---
+++ lesson18_2_ErrorHandlingMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using lesson18_2_ErrorHandlingMVC.Models;
+
+namespace lesson18_2_ErrorHandlingMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        throw new Exception();
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
