7348e5a75170bb07c61ce438d1ef6d3aac6fc501
gornvan <javanlini@gmail.com>
2024-03-18 17:32:19 +0100

    lesson19 - create mvc template to illustrate conventional routing

---
+++ lesson19_routing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lesson19_routing.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
