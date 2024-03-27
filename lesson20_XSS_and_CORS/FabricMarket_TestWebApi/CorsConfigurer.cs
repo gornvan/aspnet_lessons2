08431ff087ab685f569f93edf826bd1e122dfd6f
gornvan <javanlini@gmail.com>
2024-03-27 20:31:24 +0100

    Implement CORS configurer - read the cors setting and create policies

---
+++ lesson20_XSS_and_CORS/FabricMarket_TestWebApi/CorsConfigurer.cs
@@ -0,0 +1,44 @@
+﻿using FabricMarket_BLL.Contracts;
+using FabricMarket_TestWebApi.ConfigurationSections;
+using System.Data;
+
+namespace FabricMarket_TestWebApi
+{
+    public static class CorsConfigurer
+    {
+        public const string StrictCorsPolicyName = "MyStrictCorsPolicy";
+        public const string RelaxedCorsPolicyName = "AllowAll";
+
+        public static void Configure(WebApplicationBuilder builder)
+        {
+            var corsConfig = builder.Configuration.GetSection(nameof(CorsConfiguration)).Get<CorsConfiguration>();
+            if(corsConfig == null) {
+                throw new Exception("No CORS config section found!");
+            }
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(StrictCorsPolicyName,
+                                      policy =>
+                                      {
+                                          policy
+                                            .WithOrigins(corsConfig.AllowedOrigins.ToArray())
+                                            .WithHeaders(corsConfig.AllowedHeaders.ToArray())
+                                            .WithMethods(corsConfig.AllowedMethods.ToArray());
+                                      });
+            });
+
+
+            // relaxed policy
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyMethod();
+                    policy.AllowAnyOrigin();
+                });
+            });
+        }
+    }
+}
