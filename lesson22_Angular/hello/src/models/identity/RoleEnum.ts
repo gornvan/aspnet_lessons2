6727c9864e76ee69f06cd0da043eadee1a719e72
gornvan <javanlini@gmail.com>
2024-04-08 20:07:29 +0200

    Create a model for User and for its Role Enum

---
+++ lesson22_Angular/hello/src/models/identity/RoleEnum.ts
@@ -0,0 +1,7 @@
+export enum UserRoleEnum
+{
+    Customer = 1,
+    Cashier = 2,
+    Manager = 3,
+    Administrator = 4,
+}
---
+++ lesson22_Angular/hello/src/models/identity/UserBriefModel.ts
@@ -0,0 +1,17 @@
+import { UserRoleEnum } from "./RoleEnum";
+
+export class UserBriefModel{
+    public static FullName(user: UserBriefModel): string {
+        return  user.FirstName + ' ' + user.LastName;
+    }
+
+    constructor(
+        public Id: string,
+        public FirstName: string,
+        public LastName: string,
+        public Role: UserRoleEnum,
+        public FullName: string,
+    )
+    {
+    }
+}
