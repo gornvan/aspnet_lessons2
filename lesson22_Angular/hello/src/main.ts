5aee21ba54e52237469529e6892d2377d4e195b9
gornvan <javanlini@gmail.com>
2024-04-08 20:06:34 +0200

    Use the new AppModule instead of the templated AppComponent

--- lesson22_Angular/hello/src/main.ts
+++ lesson22_Angular/hello/src/main.ts
@@ -1,6 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+
+  // Otherwise, log the boot error
+}).catch(err => console.error(err));
