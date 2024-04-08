1d73ab84a7871ef6f832b34c26a89a3877357651
gornvan <javanlini@gmail.com>
2024-04-08 20:09:25 +0200

    cleanup AppComponent

--- lesson22_Angular/hello/src/app/app.component.ts
+++ lesson22_Angular/hello/src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { BoldDirective } from '../styling-directives/bold.style-directive'
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, BoldDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'hello';
