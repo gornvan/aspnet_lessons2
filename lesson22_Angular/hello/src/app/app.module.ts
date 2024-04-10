cd68e7ed27a5c54768d18b17280ce4689a16535f
gornvan <javanlini@gmail.com>
2024-04-10 19:43:09 +0200

    use LogIn component, instead of User List for now

--- lesson22_Angular/hello/src/app/app.module.ts
+++ lesson22_Angular/hello/src/app/app.module.ts
@@ -10,6 +10,20 @@ import { RouterOutlet } from '@angular/router';
 import { UserListComponent } from './users/user-list/user-list.component';
 import { IUserService } from './users/user-list/user-list-service.interface';
 import { UserService } from './users/user-list/user-list-service.service';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { LoginComponent } from './auth/login/loginForm.component';
+import { ILoginService } from './auth/login/login.service.interface';
+import { LoginService } from './auth/login/login.service';
+
+// material:
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; // Import MatProgressSpinnerModule
+import { MatButtonModule } from '@angular/material/button';
+
 
 /***
  * Inside the module, you need import the httpModule
@@ -22,14 +36,27 @@ import { UserService } from './users/user-list/user-list-service.service';
     CommonModule,
     RouterOutlet,
     BoldDirective,
+
+    // material:
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatInputModule,
+    MatButtonModule,
   ],
-  declarations: [AppComponent, UserListComponent],
+  declarations: [AppComponent, LoginComponent, UserListComponent],
   bootstrap: [AppComponent],
   providers: [
     {
       provide: IUserService,
       useClass: UserService,
-    }
+    },
+    {
+      provide: ILoginService,
+      useClass: LoginService,
+    },
+    provideAnimationsAsync()
   ]
 })
 export class AppModule { }
