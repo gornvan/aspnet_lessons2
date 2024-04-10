64cb0832af9af619380f623eb39b85afaeb5e7fd
gornvan <javanlini@gmail.com>
2024-04-10 21:11:54 +0200

    UserList component: retreat (navigate) to Login page in case the fetching fails with 401 (Unauthorized)

--- lesson22_Angular/hello/src/app/users/user-list/user-list.component.ts
+++ lesson22_Angular/hello/src/app/users/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserBriefModel } from '../../../models/identity/UserBriefModel';
 import { IUserService } from './user-list-service.interface';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-user-list',
@@ -9,21 +10,31 @@ import { IUserService } from './user-list-service.interface';
 export class UserListComponent implements OnInit {
     users: UserBriefModel[] = [];
 
-    constructor(private userService: IUserService) { }
+    constructor(
+        private router: Router,
+        private userService: IUserService
+    ) { }
 
     ngOnInit(): void {
         this.loadUsers();
     }
 
     loadUsers(): void {
-        this.userService.getUsers().subscribe(users => {
-            this.users = users.map(
-                (u) => {
-                    u.FullName = UserBriefModel.FullName(u);
-                    return u;
-                });
-            const _document: any = document;
-            _document.users = users;
-        });
+        this.userService.getUsers().subscribe(
+            users => {
+                this.users = users.map(
+                    (u) => {
+                        u.FullName = UserBriefModel.FullName(u);
+                        return u;
+                    });
+                const _document: any = document;
+                _document.users = users;
+            },
+            error => {
+                if(error.error && error.error.Status === 401){
+                    this.router.navigate(['login']);
+                }
+            }
+        );
     }
 }
