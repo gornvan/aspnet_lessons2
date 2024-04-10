c0ba3affaa0582f46a975ece1f54f32aa15bacd9
gornvan <javanlini@gmail.com>
2024-04-10 19:46:28 +0200

    Use the new Configuration fashion inside UserListService

--- lesson22_Angular/hello/src/app/users/user-list/user-list-service.service.ts
+++ lesson22_Angular/hello/src/app/users/user-list/user-list-service.service.ts
@@ -3,14 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserBriefModel } from '../../../models/identity/UserBriefModel';
 import { IUserService } from './user-list-service.interface';
-
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService implements IUserService {
 
-    private readonly baseUrl: string = 'https://localhost:7255'; // Update with your base URL
+    private readonly baseUrl: string = environment.API_URL;
 
     constructor(private http: HttpClient) { }
 
