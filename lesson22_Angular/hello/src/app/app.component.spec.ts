a042117ccee9b3049c0c5c5eaf3e114b887e2916
gornvan <javanlini@gmail.com>
2024-04-15 21:35:01 +0200

    Create (fix) tests for AppComponent

--- lesson22_Angular/hello/src/app/app.component.spec.ts
+++ lesson22_Angular/hello/src/app/app.component.spec.ts
@@ -1,10 +1,43 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './routing/app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { BoldDirective } from '../styling-directives/bold.style-directive';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatInputModule } from '@angular/material/input';
+
 import { AppComponent } from './app.component';
+import { UserServiceMock } from '../mocksServices/users/UserServiceMock';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { IUserService } from './users/user-list/user-list-service.interface';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppRoutingModule,
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        CommonModule,
+        BoldDirective,
+        // material:
+        MatCardModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatProgressSpinnerModule,
+        MatInputModule,
+        MatButtonModule,],
+      declarations: [AppComponent, UserListComponent],
+      providers: [{
+        provide: IUserService,
+        useClass: UserServiceMock,
+      }],
     }).compileComponents();
   });
 
@@ -14,16 +47,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hello' title`, () => {
+  it(`should have the 'Fabric Market' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hello');
+    expect(app.title).toEqual('Fabric Market');
   });
 });
