7242654ec742cee9ba9d27e9edb1a8d495d1efb1
gornvan <javanlini@gmail.com>
2024-04-24 20:31:34 +0200

    Create default MVC project with Authentication on Individual Accounts

---
+++ lesson25_OAuth_Google/MVC_With_Google_Auth/MVC_With_Google_Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_With_Google_Auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
